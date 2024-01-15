76df5d24a79f3b3327ef431abfc1a5eb04105fb4
romulo.banares <romulo88@hotmail.com>
2024-01-15 18:23:15 +0100

    add entry cards

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,4 +1,29 @@
-const NewUserPage = () => {
+import { prisma } from "@/utils/db"
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+  console.log('USER>>>>>>>>>', user)
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    })
+  }
+  redirect('/journal')
+}
+
+const NewUserPage = async () => {
+  await createNewUser()
   return <div>New User Page</div>
 }
 
