f97fe711cd111387c9e7875d29155cae8b3cd28e
romulo.banares <romulo88@hotmail.com>
2024-01-16 18:09:25 +0100

    add link to entry card

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -2,8 +2,9 @@ const EntryCard = ({ entry }) => {
     const date = new Date(entry.createdAt).toDateString()
     return (
       <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-        <div className="px-4 py-5 sm:px-6">{date}</div>
-        <div className="px-4 py-5 sm:p-6">{entry.id}</div>
+        <div className="px-4 py-5">{date}</div>
+        <div className="px-4 py-5">summary</div>
+        <div className="px-4 py-4">mood</div>
       </div>
     )
   }
