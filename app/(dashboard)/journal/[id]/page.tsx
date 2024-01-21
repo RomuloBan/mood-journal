d2faf3e6cd3c04c8bb808d879673d0ae6d909914
romulo.banares <romulo88@hotmail.com>
2024-01-21 18:59:32 +0100

    add autosave and analysis ui

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -17,9 +17,47 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id)
+  const analysisData = [
+    {
+      name: 'Summary',
+      value: '',  
+    },
+    {
+      name: 'Subject',
+      value: '',
+    },
+    {
+      name: 'Mood',
+      value: '',
+    },
+    {
+      name: 'Negative',
+      value: 'False',
+    }
+  ]
   return (
-    <div className="w-full h-full">
-      <Editor entry={entry}/>
+    <div className="w-full h-full grid grid-cols-3">
+      <div className="col-span-2">
+        <Editor entry={entry}/>
+      </div>
+      <div className="border-l border-black/10">
+        <div className="bg-blue-300 px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((data) => (
+              <li 
+                className="px-2 py-4 flex items-center justify-between border-b border-t border-black/10" 
+                key={data.name}
+              >
+                <span className="text-lg font-semibold">{data.name}</span>
+                <span>{data.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,11 +1,25 @@
 'use client'
 
+import { updateEntry } from "@/utils/api"
+import { data } from "autoprefixer"
+import { on } from "events"
 import { useState } from "react"
+import { useAutosave } from "react-autosave"
 
-const Editor = ({entry}) => {
+const Editor = ({ entry }) => {
   const [content, setContent] = useState(entry.content)
+  const [isLoading, setIsLoading] = useState(false)
+  useAutosave({
+    data: content,
+    onSave: async (_value) => {
+      setIsLoading(true)
+      const updated = await updateEntry(entry.id, _value)
+      setIsLoading(false)
+    },
+  })
   return (
     <div className="w-full h-full">
+      {isLoading && <div>Updating...</div>}
       <textarea 
         className="w-full h-full p-8 text-xl outline-none" 
         value={content} 
