ca80d605115c673cd857006a1ffc1dc3ca3ea77c
romulo.banares <romulo88@hotmail.com>
2024-01-21 18:18:40 +0100

    add update entry api and route

--- utils/api.ts
+++ utils/api.ts
@@ -2,6 +2,20 @@ const createURL = (path: string): string => {
   return `${window.location.origin}${path}`
 }
 
+export const updateEntry = async (id: string, content: string) => {
+  const res = await fetch(
+    new Request(createURL(`/api/journal/${id}`), {
+      method: 'PATCH',
+      body: JSON.stringify({ content }),
+    })
+  )
+
+  if (res.ok) {
+    const { data } = await res.json()
+    return data
+  }
+}
+
 export const createNewEntry = async () => {
   const response = await fetch(createURL("/api/journal"), {
       method: "POST",
