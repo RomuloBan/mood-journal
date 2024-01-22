4091a7b2eb114739b3821c9dd9f194f5bcb53bdb
romulo.banares <romulo88@hotmail.com>
2024-01-22 20:36:57 +0100

    add template format

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -15,22 +15,13 @@ const getEntries = async () => {
       createdAt: "desc",
     },
   })
-  const entry = `I'm going  to give you a journal entry to analyze for a few things.
-  I want you to tell me what the subject of the entry is, what the mood of the entry is,
-   and whether or not the entry is negative and a color representing the mood of the entry.
-  You need to respond back with formated JSON that looks like this:
-   { "mood": "", "subject": "", "color": "", "negative": ""}.
-
-  Here's the entry: 
-  Today was a really great day. I finally was able  to grab that pair of shoes I've been dying to get.
-  `  
+  const entry = `Today was a eh, ok day I guess. I found a new coffee shop that was cool but then I got a flat tire. :)`  
   await analize(entry)
   return entries
 }
 
 const JournalPage = async () => {
   const entries = await getEntries()
-  console.log('entries', entries)
   return (
     <div className="p-10 bg-zinc-400/10 h-full">
       <h2 className="text-3xl mb-8">Journal</h2>
--- utils/ai.ts
+++ utils/ai.ts
@@ -1,12 +1,14 @@
 import { OpenAI } from '@langchain/openai'
 import { z } from 'zod'
 import { StructuredOutputParser } from 'langchain/output_parsers'
+import { PromptTemplate } from '@langchain/core/prompts'
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
     mood: z
       .string()
       .describe('the mood of the person who wrote the journal entry.'),
+      subject: z.string().describe('the subject of the journal entry.'),
     summary: z
       .string()
       .describe('quick summary of the entire entry.'),
@@ -20,8 +22,25 @@ const parser = StructuredOutputParser.fromZodSchema(
       ),
   })
 )
+
+const getPrompt = async (content: any) => {
+  const format_instructions = parser.getFormatInstructions()
+
+  const prompt = new PromptTemplate({
+    template: 
+      'Analyze the following journal entry. Follow the instructions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}',
+    inputVariables: ['entry'],
+    partialVariables: { format_instructions },
+  })
+
+  const input = await prompt.format({ 
+    entry: content 
+  })
+  return input
+}
+
 export const analize = async (prompt: string) => {
+  const input = await getPrompt(prompt)
   const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo'})
-  const result = await model.invoke(prompt)
-  console.log('>>>>>>', result)
+  const result = await model.invoke(input)
 }
