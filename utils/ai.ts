5b2adfc4d43cf18bb8b37730d84a7d4feca56009
romulo.banares <romulo88@hotmail.com>
2024-01-22 19:59:09 +0100

    add zod schema

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,6 +1,27 @@
 import { OpenAI } from '@langchain/openai'
+import { z } from 'zod'
+import { StructuredOutputParser } from 'langchain/output_parsers'
+
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    mood: z
+      .string()
+      .describe('the mood of the person who wrote the journal entry.'),
+    summary: z
+      .string()
+      .describe('quick summary of the entire entry.'),
+    negative: z
+      .boolean()
+      .describe('is the journal entry negative? (i.e. does it contain negative emotions?).'),
+    color: z
+      .string()
+      .describe(
+        'a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
+      ),
+  })
+)
 export const analize = async (prompt: string) => {
-    const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo'})
-    const result = await model.invoke(prompt)
-    console.log('>>>>>>', result)
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo'})
+  const result = await model.invoke(prompt)
+  console.log('>>>>>>', result)
 }
